a7450ba27f81b2b95ebff555d985f3f7f434b23e
venkatesh <yangalavenkatesh15052002@gmail.com>
2022-03-09 14:58:48 +0530

    09-03-2022 class object commited

---
+++ 1pgrm.js
@@ -0,0 +1,18 @@
+class student
+{
+    sname="venky";
+    clg="cvr";
+     rollno=1254;
+     display()
+     {
+         console.log("Student name is "+this.sname);
+     }
+    }
+
+   let s=new student();
+     s.display();
+
+
+
+
+
